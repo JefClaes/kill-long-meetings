60cb63a58e093b1acee65d551d9a77b40ebc8471
Jef Claes <jef.claes@gmail.com>
2012-10-23 21:36:47 +0200

    NancyFx hello world

---
+++ src/MeetingCostTicker/MeetingCostTicker/RootModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace MeetingCostTicker
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule()
+        {
+            Get["/"] = p => "Hello World";
+        }
+    }
+}
